76758b752e78abab9650a285d1e82c996ced8aae
2markit <jakemckenzie19@gmail.com>
2018-12-18 08:02:42 -0800

    Add files via upload

--- contactengine.php
+++ contactengine.php
@@ -1,8 +1,7 @@
 <?php
-
-$EmailFrom = "jakemckenzie19@gmail.com";
+$EmailFrom = "info@blank.inc";
 $EmailTo = "jakemckenzie19@gmail.com";
-$Subject = "Test email";
+$Subject = "Blank Inc. Contact Form";
 $Name = Trim(stripslashes($_POST['Name']));
 $Tel = Trim(stripslashes($_POST['Tel']));
 $Email = Trim(stripslashes($_POST['Email']));
@@ -36,8 +35,15 @@ $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 // redirect to success page
 if ($success){
   print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+	<script language="javascript" type="text/javascript">
+    alert('Thank you for your correspondence. We will contact you shortly.');
+    window.location = 'index.html';
+	</script>
 }
 else{
   print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+	<script language="javascript" type="text/javascript">
+    alert('Message failed. Please, send an email to info@blank.inc');
+	</script>
 }
 ?>
